ccf201bd0fd1ec65cae5aa544d3d3d40b5ae7f35
Tim <tim7777700000@gmail.com>
2021-04-02 15:51:16 +0800

    Test Create, Edit, Details, Delete Success

---
+++ TaiwanStockExchangeData/Migrations/20210402073816_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaiwanStockExchangeData.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Security",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CodeName = table.Column<long>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    DividendYield = table.Column<double>(type: "REAL", nullable: false),
+                    DividendYear = table.Column<long>(type: "INTEGER", nullable: false),
+                    PriceToEarningRatio = table.Column<double>(type: "REAL", nullable: false),
+                    PriceToBookRatio = table.Column<double>(type: "REAL", nullable: false),
+                    FinancialStatements = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Security", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Security");
+        }
+    }
+}
